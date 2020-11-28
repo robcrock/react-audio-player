4f717c9d956e9050229b6de7a172ca6fde1a85e8
robcrockPI <robert@propellerindustries.com>
2020-11-28 11:16:34 -0300

    Made the input draggable.

--- src/components/Player.js
+++ src/components/Player.js
@@ -31,6 +31,10 @@ export default function Player({ currentSong, isPlaying, setIsPlaying }) {
     let seconds = ("0" + Math.floor(time % 60)).slice(-2)
     return `${minutes}:${seconds}`
   }
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value
+    setSongInfo({ ...songInfo, currentTime: e.target.value })
+  }
 
   // State
   const [songInfo, setSongInfo] = useState({
@@ -42,7 +46,13 @@ export default function Player({ currentSong, isPlaying, setIsPlaying }) {
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
